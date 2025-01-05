ec6c11cb811d358963154afd44a41180733553e5
홍현정 <emilyhong4659@gmail.com>
2025-01-05 23:52:54 +0900

    Add Baekjoon level3 problems

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-//    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
   }
 }
